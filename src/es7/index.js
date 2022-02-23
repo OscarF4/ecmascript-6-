9457d663b7ce43ae4431d0e1b40eab1f7be2079a
oscarf4 <oscarvedo97@gmail.com>
2022-02-23 16:16:23 -0500

    Actualización ES7: includes() y potencias **

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+//includes(): Saber si elemento está dentro de un Array
+let numbers = [1,2,3,7,8];
+if (numbers.includes(7)) {
+    console.log('Sí se encuentra el valor 7');
+} else {
+    console.log('No se encuentra');
+}
+
+//Elevaciones de potencia
+let base = 4;
+let exponente = 3;
+let result = base ** exponente;
+
+console.log(result);
