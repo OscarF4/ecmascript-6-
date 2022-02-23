ac8937348b5a7590fb155b916bf36b567cdbb894
oscarf4 <oscarvedo97@gmail.com>
2022-02-23 11:23:30 -0500

    Promesas en es6

--- src/es6/index.js
+++ src/es6/index.js
@@ -9,23 +9,26 @@ function newFunction(name, age, country) {
 }
 newFunction();
 
-//ES6: Desde la asignación de los parámetros
+//es6: Desde la asignación de los parámetros
 function newFunctionES6(name = 'Oscar', age = 24, country = 'COL') {
     console.log(name, age, country);
 }
 newFunctionES6();
 newFunctionES6('Ricardo', 33, 'MX');
 
+
 //Concatenación con Template Strings
 let myName = 'Oscar';
 let lastName = 'Acevedo';
 console.log(`Mi nombre es: ${myName} ${lastName}`);
+
 
 // Multilinea: Para tener saltos de línea y no escribir "\n"
 //Con los Template Literals viene por defecto el comportamiento
 let lorem = `Hola, esta frase necesita un salto de linea.
 Aquí inicia otra línea.`
 console.log(lorem);
+
 
 //Desestructuración: Con esto, podemos crear en una sola línea varias variables que vengan de un mismo objeto.
 let person = {
@@ -45,6 +48,7 @@ let education = ['David', ...team1, ...team2];
 
 console.log(education); // [David, Oscar, Julian...]
 
+
 //Objetos
 let name = 'Oscar';
 let age = 24;
@@ -54,6 +58,7 @@ obj = { name: name, age: age };
 objECMA = { name, age }
 //es6
 console.log(objECMA);
+
 
 //Arrow Functions: Funciones anónimas
 const names = [
@@ -66,3 +71,19 @@ console.log(listOfNames);
 const listOfNames2 = (name, age, country) => {};
 //un solo parámetro
 const listOfNames2 = name => {};
+
+
+//Promises: Asincronismo (resolve y reject)
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!');
+        } else {
+            reject('Ups!');
+        }
+    });
+};
+
+helloPromise()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
